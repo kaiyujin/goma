8c4f9ede20866b897dc38c0731e59c023634b294
daisuke shiraishi <kaiyujin@gmail.com>
2019-03-10 23:22:48 +0900

    add todo

--- sql_file.go
+++ sql_file.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 )
 
+// TODO cache
 func get(path string) (data string, err error) {
 	result := []byte(nil)
 	result, err = ioutil.ReadFile(path)
 	return fmt.Sprintf("%s", result), err
 }
+
+// replace named key
